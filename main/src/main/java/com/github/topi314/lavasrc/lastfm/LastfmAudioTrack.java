b774951c4e3c823bb0d0e8befdd0deefb55b2302
asyncio <134554554+asynico@users.noreply.github.com>
2025-07-14 19:17:56 -0400

    Update LastfmAudioTrack.java

--- main/src/main/java/com/github/topi314/lavasrc/lastfm/LastfmAudioTrack.java
+++ main/src/main/java/com/github/topi314/lavasrc/lastfm/LastfmAudioTrack.java
@@ -2,25 +2,25 @@ package com.github.topi314.lavasrc.lastfm;
 
 import com.github.topi314.lavasrc.mirror.MirroringAudioTrack;
 import com.github.topi314.lavasrc.mirror.MirroringAudioSourceManager;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.InternalAudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
-import com.sedmelluq.discord.lavaplayer.container.common.SeekableInputStream;
+import com.sedmelluq.discord.lavaplayer.tools.io.SeekableInputStream;
 
 public class LastfmAudioTrack extends MirroringAudioTrack {
 
     public LastfmAudioTrack(AudioTrackInfo trackInfo, MirroringAudioSourceManager sourceManager) {
         super(trackInfo, 
               trackInfo.artworkUrl,    
-              trackInfo.title,       
+              trackInfo.title,         
               trackInfo.author,       
-              trackInfo.uri,          
-              trackInfo.identifier,   
+              trackInfo.uri,           
+              trackInfo.identifier,  
               trackInfo.isStream,      
-              sourceManager);      
+              sourceManager);          
     }
 
     @Override
-    public AudioTrack createAudioTrack(AudioTrackInfo trackInfo, SeekableInputStream inputStream) {
+    public InternalAudioTrack createAudioTrack(AudioTrackInfo trackInfo, SeekableInputStream inputStream) {
         return new LastfmAudioTrack(trackInfo, (MirroringAudioSourceManager) this.sourceManager);
     }
 }
